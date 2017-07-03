01a6f77f472c5dbd63da4f40ce952fc8ea61a3a3
Unknown <bgoldstein93@gmail.com>
2017-07-03 20:37:29 +0300

    Add more complete implementation to matrix.

--- python/matrix.py
+++ python/matrix.py
@@ -2,14 +2,88 @@
 #
 # Author: Bradley Goldstein
 
+import random
+
+import numpy as np
+
 class Matrix(object):
   def __init__(self, num_rows, num_columns):
     self.matrix_ = []
     for x in range(num_rows):
       self.matrix_.append([0 for y in range(num_columns)])
 
+  def __nonzero__(self):
+    return self.matrix_ != []
+
+  def empty(self):
+    return not self.__nonzero__()
+
+  def __eq__(self, other):
+    if not isinstance(other, Matrix):
+      return False
+
+    if self.empty():
+      return other.empty()
+
+    if self.numRows() is not other.numRows() or self.numCols() is not other.numCols():
+      return False
+
+    for i in range(self.numRows()):
+      for j in range(self.numCols()):
+        if self[i][j] != other[i][j]:
+          return False
+
+    return True
+
   def __getitem__(self, column_index):
     return self.matrix_[column_index]
+
+  def numRows(self):
+    return 0 if self.empty() else len(self.matrix_)
+
+  def numCols(self):
+    return 0 if self.empty() else len(self.matrix_[0])
+
+  @classmethod
+  def fromRows(cls, rows):
+    m = cls(0, 0)
+    for row in rows:
+      m._appendRow(row)
+    return m
+
+  @classmethod
+  def columnVector(cls, col):
+    m = cls(0, 0)
+    m._appendCol(col)
+    return m
+
+  @classmethod
+  def rowVector(cls, row):
+    m = cls(0, 0)
+    m._appendRow(row)
+    return m
+
+  @classmethod
+  def randomMatrix(cls, num_rows, num_columns, min_val=0, max_val=10):
+    m = cls(num_rows, num_columns)
+    for i in range(num_rows):
+      for j in range(num_columns):
+        m.matrix_[i][j] = random.randint(min_val, max_val)
+    return m
+
+  def _appendRow(self, row):
+    if not self.empty():
+      assert len(row) is self.numCols(), 'Row to append must be the same width as the matrix.'
+    self.matrix_.append(row)
+
+  def _appendCol(self, col):
+    if  self.empty():
+      for val in col:
+        self.matrix_.append([val])
+      return
+    assert len(col) is self.numRows(), 'Column to append must be the same height as the matrix.'
+    for i, val in enumerate(col):
+      self.matrix_[i].append(val)
 
   def __mul__(self, other):
     if isinstance(other, int) or isinstance(other, float):
@@ -26,16 +100,15 @@ class Matrix(object):
     # Other types of Matrix multiplication are undefined
     raise TypeError
 
-  def numRows(self):
-    return len(self.matrix_)
-
-  def numCols(self):
-    return len(self.matrix_[0])
+  def getEquivalentNumpyMatrix(self):
+    return np.array(self.matrix_)
 
   def matrixMultiply(self, other):
     if self.numCols() != other.numRows():
-      raise ValueError('Attempting to multiple M1 * M2 where M1.num_cols != M2.num_rows')
+      raise ValueError('Attempting to matrix multiply where M1.num_cols != M2.num_rows')
 
+    # if self.numCols() == self.numRows() == other.numCols() == other.numRows() and powerOf2(self.numRows()):
+    #   return self._squareMatrixMultiply(other)
     # Naive algorithm to multiply matrices: 'Square-Matrix-Mutliply'
     #
     # See p.75-76 in CLRS 3rd ed.
@@ -47,6 +120,3 @@ class Matrix(object):
           cell_i_j += self[i][k] * other[k][j]
         out[i][j] = cell_i_j
     return out
-
-# August 4-26
-# 7.5 days off
--- python/matrix_test.py
+++ python/matrix_test.py
@@ -6,51 +6,132 @@ import matrix
 
 import unittest
 
+import numpy as np
+
 
 class TestMatrix(unittest.TestCase):
-  def testMatrix_instantiation_createsEmptyMatrix(self):
-    m = matrix.Matrix(2, 3)
-    self.assertEqual(m[0], [0, 0, 0])
-    self.assertEqual(m[1], [0, 0, 0])
-    self.assertEqual(m[0][0], 0)
-    self.assertEqual(m[1][2], 0)
+  def testTruthiness_emptyMatrix_returnsCorrectValues(self):
+    m = matrix.Matrix(0, 0)
+    m.matrix_ = []
 
-  def testMatrix_rowOutOfBounds_throwsIndexError(self):
-    m = matrix.Matrix(3, 2)
-    self.assertRaises(IndexError, m.__getitem__, 3)
+    self.assertFalse(m)
+    self.assertFalse(m.__nonzero__())
+    self.assertTrue(m.empty())
 
-  def testMatrix_columnOutOfBounds_throwsIndexError(self):
-    col = matrix.Matrix(3, 2)[0]
-    self.assertRaises(IndexError, col.__getitem__, 2)
+  def testTruthiness_normalMatrix_returnsCorrectValues(self):
+    m = matrix.Matrix(0, 0)
+    m.matrix_ = [[0]]
 
-  def testMatrix_setCellInMatrix_updatesCell(self):
+    self.assertTrue(m)
+    self.assertTrue(m.__nonzero__())
+    self.assertFalse(m.empty())
+
+  def testEquals_otherIsNotAMatrix_returnsFalse(self):
+    m = matrix.Matrix(1, 1)
+
+    self.assertNotEqual(m, [[0]])
+
+  def testEquals_bothAreEmpty_returnsTrue(self):
+    m = matrix.Matrix(0, 0)
+    other = matrix.Matrix(0, 0)
+    other.matrix_ = []
+
+    self.assertEqual(m, other)
+
+  def testEquals_otherIsNotEmpty_returnsFalse(self):
+    m = matrix.Matrix(0, 0)
+    other = matrix.Matrix(1, 1)
+
+    self.assertNotEqual(m, other)
+
+  def testEquals_otherIsEmpty_returnsFalse(self):
+    m = matrix.Matrix(1, 1)
+    other = matrix.Matrix(0, 0)
+
+    self.assertNotEqual(m, other)
+
+  def testEquals_otherIsEqual_returnsTrue(self):
+    m = matrix.Matrix(1, 2)
+    m[0][1] = 'a'
+    other = matrix.Matrix(0, 0)
+    other.matrix_ = [[0, 'a']]
+
+    self.assertEqual(m, other)
+
+  def testSet_cellInMatrix_updatesCell(self):
     m = matrix.Matrix(2, 3)
     m[1][2] = -3
     m[0][2] = 'a'
     m[1][0] = 4.0
+
     self.assertEqual(m.matrix_, [[0, 0, 'a'], [4.0, 0, -3]])
 
-  def testMatrix_multiplication_doesProperMultiplication(self):
-    m1 = matrix.Matrix(2, 2)
-    m1[0][0] = 1
-    m1[0][1] = 2
-    m1[1][0] = -3
-    m1[1][1] = 0
-    # m1 = [[ 1, 2],
-    #        -3, 0]]
+  def testInitialization_zeroArguments_createsEmptyMatrix(self):
+    m = matrix.Matrix(0, 0)
 
-    m2 = matrix.Matrix(2, 3)
-    m2[0][0] = 1
-    m2[0][1] = 3
-    m2[0][2] = -1
-    m2[1][0] = 2
-    m2[1][1] = 0
-    m2[1][2] = 4
-    # m2 = [[1, 3, -1],
-    #       [2, 0,  4]]
+    self.assertTrue(m.empty())
+
+  def testInitialization_plainInitialization_createsZerosMatrix(self):
+    m = matrix.Matrix(2, 3)
+
+    self.assertEqual(m.matrix_, [[0, 0, 0], [0, 0, 0]])
+
+  def testFromRows_normalArguments_createsProperMatrix(self):
+    m = matrix.Matrix.fromRows([[1, 2], [3, 4], [5, 6]])
+
+    self.assertEqual(m.matrix_, [[1, 2], [3, 4], [5, 6]])
+
+  def testColumnVector_normalArguments_createsProperMatrix(self):
+    m = matrix.Matrix.columnVector([1, 2, 3, 4])
+
+    self.assertEqual(m.matrix_,[[1], [2], [3], [4]])
+
+  def testRowVector_normatArguments_createsProperMatrix(self):
+    m = matrix.Matrix.rowVector([1, 2, 3, 4])
+
+    self.assertEqual(m.matrix_, [[1, 2, 3, 4]])
+
+  def testGet_rowOutOfBounds_throwsIndexError(self):
+    m = matrix.Matrix(3, 2)
+
+    self.assertRaises(IndexError, m.__getitem__, 3)
+
+  def testGet_columnOutOfBounds_throwsIndexError(self):
+    col = matrix.Matrix(3, 2)[0]
+
+    self.assertRaises(IndexError, col.__getitem__, 2)
+
+  def testGetEquivalentNumpyMatrix_normalArguments_createsEquivalentMatrix(self):
+    m = matrix.Matrix.randomMatrix(num_rows=3, num_columns=4, max_val=5)
+
+    n = m.getEquivalentNumpyMatrix()
+
+    for i in range(m.numRows()):
+      for j in range(m.numCols()):
+        self.assertEqual(m[i][j], n[i][j])
+
+    # Check that changing a value in m does not change a value in n
+    m[2][2] = 'a'
+    self.assertNotEqual(m[2][2], n[2][2])
+
+  def testMultiplication_normalArguments_doesProperMultiplication(self):
+    m1 = matrix.Matrix.fromRows([[1, 2],
+                                 [-3, 0]])
+    m2 = matrix.Matrix.fromRows([[1, 3, -1],
+                                 [2, 0, 4]])
 
     self.assertEqual((m1*m2).matrix_, [[ 5,  3, 7],
                                        [-3, -9, 3]])
+
+  def testMultiplication_mismatchedRowsAndColumns_raisesValueError(self):
+    m1 = matrix.Matrix.fromRows([[1, 2],
+                                 [-3, 0]])
+    m2 = matrix.Matrix.fromRows([[1, 3],
+                                 [2, 0],
+                                 [3, 4]])
+
+    self.assertRaises(ValueError, m1.__mul__, m2)
+
 
 if __name__ == '__main__':
     unittest.main()
